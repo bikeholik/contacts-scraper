63591961aa2c8adfc5404bdb1cddfc5816980f02
bikeholik <bikeholik@gmail.com>
2019-02-18 07:18:37 +0000

    -- synchronization and ext fix

--- scraper.go
+++ scraper.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -19,10 +20,12 @@ type FoundEmail struct {
 }
 
 const RFC_MAIL_REGEXP = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])"
-const SIMPLE_MAIL_REGEXP = "[\\w]+@[\\w]+\\.[\\w]{2,}(\\.[\\w]{2,})?"
+const SIMPLE_MAIL_REGEXP = "[\\w]+@[\\w]{2,}\\.[\\w]{2,}(\\.[\\w]{2,})?"
 const MAIL_REGEXP = SIMPLE_MAIL_REGEXP
 
-var DISALLOWED_EXTENSIONS = []string{".png", ".gif", ".jpg", ".jpeg"}
+var disallowedExtensions = []string{"png", "gif", "jpg", "jpeg"}
+
+var disallowedDomainsMutex sync.Mutex
 
 func scrape(startUrl *url.URL, maxDepth int, maxDuration time.Duration, emails chan FoundEmail) {
 
@@ -91,13 +94,15 @@ func scrape(startUrl *url.URL, maxDepth int, maxDuration time.Duration, emails c
 }
 
 func blockDomain(collector *colly.Collector, r *colly.Request) {
+	disallowedDomainsMutex.Lock()
 	collector.DisallowedURLFilters = append(collector.DisallowedURLFilters, regexp.MustCompile(".*"+r.URL.Host))
+	disallowedDomainsMutex.Unlock()
 }
 
 func shouldBeIgnored(email string) bool {
 	chunks := strings.Split(email, ".")
 	ext := chunks[len(chunks)-1]
-	for _, s := range DISALLOWED_EXTENSIONS {
+	for _, s := range disallowedExtensions {
 		if s == ext {
 			return true
 		}
